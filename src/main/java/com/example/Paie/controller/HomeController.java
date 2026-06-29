504b971f31996b9073bb4d011660cc016d64ae18
Lamine-Camara809 <camara.lamine0597@gmail.com>
2026-06-29 03:09:09 +0000

    feat: ajout de HommeController

---
+++ src/main/java/com/example/Paie/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.Paie.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String Home(){
+        return "Bienvenue sur spring boot";
+    }
+}
